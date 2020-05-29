eb86d81d0e3a69aa5437e91b7e50fd75ece23382
Pat McClernan <patmccler@gmail.com>
2020-05-29 10:58:42 -0400

    tests passing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(args)
+    network = Network.new(args)
+    network.shows << self
+    network
+  end
+
+  def actors_list
+    characters.map(&:actor).map(&:full_name)
+  end
 end
