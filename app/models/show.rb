9429266191593b1d00d220e2598446e89a67b15d
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-29 10:25:45 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    list = []
+    actor_full_name = "#{characters.first.actor.first_name} #{characters.first.actor.last_name}"
+    list << actor_full_name
+    list
+  end
 end
